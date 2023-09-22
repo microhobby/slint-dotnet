d994fcd46f0c3005aff0f88c0d89df23ed57e4da
Matheus Castello <matheus@castello.eng.br>
2023-09-22 19:54:58 -0300

    test: update test to use debug build
    Also we added a thread to test the RunOnUiThread method.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>
    
    diff --git a/.vscode/tasks.json b/.vscode/tasks.json
    index acf02e7..d800e1e 100644
    --- a/.vscode/tasks.json
    +++ b/.vscode/tasks.json
    @@ -201,6 +201,31 @@
                 "group": "build",
                 "dependsOrder": "sequence"
             },
    +        {
    +            "label": "rnet-gen-debug",
    +            "hide": true,
    +            "command": "rnet-gen",
    +            "type": "shell",
    +            "options": {
    +            },
    +            "args": [
    +                "${workspaceFolder}/target/debug/libslint_dotnet.so",
    +                ">",
    +                "${workspaceFolder}/target/debug/libslint_dotnet.cs",
    +            ],
    +            "problemMatcher": [
    +                "$rustc"
    +            ],
    +            "presentation": {
    +                "reveal": "always"
    +            },
    +            "icon": {
    +                "id": "flame",
    +                "color": "terminal.ansiYellow"
    +            },
    +            "group": "build",
    +            "dependsOrder": "sequence"
    +        },
             {
                 "label": "rnet-replace",
                 "hide": true,
    diff --git a/test/Program.cs b/test/Program.cs
    index 7b65d59..26fc1fa 100644
    --- a/test/Program.cs
    +++ b/test/Program.cs
    @@ -10,43 +10,71 @@ Console.WriteLine("Getting properties...");
     var props = Slint.GetProperties();
     Console.WriteLine("Geted");
    
    -foreach(var prop in props) {
    +foreach (var prop in props)
    +{
         Console.WriteLine($"Name: {prop.typeName}");
         Console.WriteLine($"Type: {prop.typeType}");
         Console.WriteLine($"Val: {prop.typeValue}");
     }
    
    -Slint.SetProperty(new Slint.DotNetValue {
    +Slint.SetProperty(new Slint.DotNetValue
    +{
         typeName = "img",
         typeType = 3,
         typeValue = "./ui/assets/torizon_logo_white.svg"
     });
    
    -Slint.SetCallback("request-increase-value", () => {
    +Slint.SetCallback("request-increase-value", () =>
    +{
         var dt = Slint.GetProperty("counter");
         var sT = dt.typeValue
                     .Replace("Value::Number(", "")
                     .Replace(")", "");
         var val = float.Parse(sT) + 1;
    -
    -    Slint.SetProperty(new Slint.DotNetValue {
    +
    +    Slint.SetProperty(new Slint.DotNetValue
    +    {
             typeName = "counter",
             typeType = 1,
             typeValue = val.ToString()
         });
    
    -    Slint.SetProperty(new Slint.DotNetValue {
    +    Slint.SetProperty(new Slint.DotNetValue
    +    {
             typeName = "img",
             typeType = 3,
             typeValue = "./ui/assets/toradex_logo.png"
         });
    -
    +
         return true;
     });
    
    -Slint.NewTimer(1, 500, () => {
    +Slint.NewTimer(1, 500, () =>
    +{
         Console.WriteLine("This was the timer...");
         return true;
     });
    
    +new Thread(() =>
    +{
    +    while (true)
    +    {
    +        Thread.Sleep(10000);
    +
    +        Slint.RunOnUiThread(() =>
    +        {
    +            Console.WriteLine("This was the UI thread...");
    +
    +            Slint.SetProperty(new Slint.DotNetValue
    +            {
    +                typeName = "counter",
    +                typeType = 1,
    +                typeValue = 0.ToString()
    +            });
    +
    +            return true;
    +        });
    +    }
    +}).Start();
    +
     Slint.Run();
    diff --git a/test/test.csproj b/test/test.csproj
    index b03c7bb..322d1f4 100644
    --- a/test/test.csproj
    +++ b/test/test.csproj
    @@ -9,12 +9,12 @@
         </PropertyGroup>
    
         <ItemGroup>
    -        <Compile Include="../target/release/libslint_dotnet.cs" />
    +        <Compile Include="../target/debug/libslint_dotnet.cs" />
         </ItemGroup>
    
         <!-- rule to copy the .so to the bin folder -->
         <Target Name="CopyNativeLibs" BeforeTargets="Build">
    -        <Copy SourceFiles="$(ProjectDir)../target/release/libslint_dotnet.so" DestinationFolder="$(OutputPath)" />
    +        <Copy SourceFiles="$(ProjectDir)../target/debug/libslint_dotnet.so" DestinationFolder="$(OutputPath)" />
         </Target>
    
         <!-- copy the ui folder -->

--- test/Program.cs
+++ test/Program.cs
@@ -10,43 +10,74 @@ Console.WriteLine("Getting properties...");
 var props = Slint.GetProperties();
 Console.WriteLine("Geted");
 
-foreach(var prop in props) {
+foreach (var prop in props)
+{
     Console.WriteLine($"Name: {prop.typeName}");
     Console.WriteLine($"Type: {prop.typeType}");
     Console.WriteLine($"Val: {prop.typeValue}");
 }
 
-Slint.SetProperty(new Slint.DotNetValue {
+Slint.SetProperty(new Slint.DotNetValue
+{
     typeName = "img",
     typeType = 3,
     typeValue = "./ui/assets/torizon_logo_white.svg"
 });
 
-Slint.SetCallback("request-increase-value", () => {
+Slint.SetCallback("request-increase-value", () =>
+{
     var dt = Slint.GetProperty("counter");
     var sT = dt.typeValue
                 .Replace("Value::Number(", "")
                 .Replace(")", "");
     var val = float.Parse(sT) + 1;
-    
-    Slint.SetProperty(new Slint.DotNetValue {
+
+    Slint.SetProperty(new Slint.DotNetValue
+    {
         typeName = "counter",
         typeType = 1,
         typeValue = val.ToString()
     });
 
-    Slint.SetProperty(new Slint.DotNetValue {
+    Slint.SetProperty(new Slint.DotNetValue
+    {
         typeName = "img",
         typeType = 3,
         typeValue = "./ui/assets/toradex_logo.png"
     });
-    
+
     return true;
 });
 
-Slint.NewTimer(1, 500, () => {
+Slint.NewTimer(1, 500, () =>
+{
     Console.WriteLine("This was the timer...");
     return true;
 });
 
+new Thread(() =>
+{
+    while (true)
+    {
+        Console.WriteLine("Thread Id: {0}", Environment.CurrentManagedThreadId);
+        Thread.Sleep(10000);
+
+        Slint.RunOnUiThread(() =>
+        {
+            Console.WriteLine("Thread Id: {0}", Environment.CurrentManagedThreadId);
+            Console.WriteLine("This was the UI thread...");
+
+            Slint.SetProperty(new Slint.DotNetValue
+            {
+                typeName = "counter",
+                typeType = 1,
+                typeValue = 0.ToString()
+            });
+
+            return true;
+        });
+    }
+}).Start();
+
+Console.WriteLine("Thread Id: {0}", Environment.CurrentManagedThreadId);
 Slint.Run();
