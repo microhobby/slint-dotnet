a9cd66a73380eb34bd4b1491cccafa643e3c4c7c
Matheus Castello <matheus.castello@toradex.com>
2023-12-31 16:37:23 -0300

    SlintDotnet: Bump to version 1.2.28
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- SlintDotnet/Generator.cs
+++ SlintDotnet/Generator.cs
@@ -10,7 +10,7 @@ namespace SlintDotnet.SourceGenerator;
 public class Generator : ISourceGenerator
 {
     // TODO: do not forget to update the version
-    private static string PACKAGE_VERSION = "1.2.26";
+    private static string PACKAGE_VERSION = "1.2.28";
 
     protected struct struct_info {
         public int index;
